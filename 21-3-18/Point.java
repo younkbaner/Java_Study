6fb6c3d67308b19670ee67d4b189d5e721a24066
wenzhong <599508997@qq.com>
2021-08-02 17:55:12 +0800

    123

--- 21-3-18/Point.java
+++ 21-3-18/Point.java
@@ -69,6 +69,7 @@ public class Point{
 	}
 	
 	void showArguement(String... args){
+		System.out.println("你好");
 		for(int i=0;i<args.length;i++){
 			System.out.println("第"+(i+1)+"个参数为:"+args[i]);
 		}
